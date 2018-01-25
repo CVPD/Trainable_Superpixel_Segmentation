0c65ffc914c75eccf44edc762d2e48d6562efa1b
Josu <96jsalinas@gmail.com>
2018-01-25 13:43:51 +0200

    Fixed test class to use show the ResultsTable instead of printing it using a String

--- src/test/java/trainableSuperpixelSegmentation/testSuperpixelSegmentation.java
+++ src/test/java/trainableSuperpixelSegmentation/testSuperpixelSegmentation.java
@@ -3,6 +3,8 @@ package trainableSuperpixelSegmentation;
 import com.EHU.imagej.RegionFeatures;
 import ij.IJ;
 import ij.ImagePlus;
+import ij.measure.ResultsTable;
+import inra.ijpb.measure.IntensityMeasures;
 
 import java.util.ArrayList;
 
@@ -22,13 +24,37 @@ public class testSuperpixelSegmentation{
         selectedFeatures.add(RegionFeatures.Feature.fromLabel("Max"));
         selectedFeatures.add(RegionFeatures.Feature.fromLabel("Min"));
         RegionFeatures regionFeatures = new RegionFeatures(inputImage,labelImage,selectedFeatures);
-        System.out.println(regionFeatures.getMeanTable().toString());
-        System.out.println(regionFeatures.getMedianTable().toString());
-        System.out.println(regionFeatures.getModeTable().toString());
-        System.out.println(regionFeatures.getSkewnessTable().toString());
-        System.out.println(regionFeatures.getKurtosisTable().toString());
-        System.out.println(regionFeatures.getStdDevTable().toString());
-        System.out.println(regionFeatures.getMaxTable().toString());
-        System.out.println(regionFeatures.getMinTable().toString());
+        ArrayList<ResultsTable> results = new ArrayList<ResultsTable>();
+        final IntensityMeasures im = new IntensityMeasures( inputImage, labelImage );
+        results.add( im.getMean() );
+        results.add( im.getStdDev() );
+        results.add( im.getMax() );
+        results.add( im.getMin() );
+        results.add( im.getMedian() );
+        results.add( im.getMode() );
+        results.add( im.getSkewness() );
+        results.add( im.getKurtosis() );
+        results.add( im.getNumberOfVoxels() );
+        results.add( im.getVolume() );
+
+        ResultsTable mergedTable = new ResultsTable();
+        final int numLabels = results.get( 0 ).getCounter();
+
+        for(int i=0; i < numLabels; i++ )
+        {
+            mergedTable.incrementCounter();
+            String label = results.get( 0 ).getLabel( i );
+            mergedTable.addLabel(label);
+
+            for( int j=0; j<results.size(); j++ )
+            {
+                String measure = results.get( j ).getColumnHeading( 0 );
+                double value = results.get( j ).getValue( measure, i );
+                mergedTable.addValue( measure, value );
+            }
+        }
+
+        mergedTable.show( inputImage.getShortTitle() +
+                "-intensity-measurements" );
     }
 }
