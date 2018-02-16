d34a3a5bd847aed28d30a71df6b010c1ccd68f0b
Ignacio Arganda-Carreras <ignacio.arganda@gmail.com>
2018-02-16 18:01:50 +0100

    Use corrected label image and random forest classifier.
    After updating the labels of each class and using the new classifier, the results are much better.

--- src/test/java/eus/ehu/tss/TestColorSuperpixelSegmentation.java
+++ src/test/java/eus/ehu/tss/TestColorSuperpixelSegmentation.java
@@ -3,6 +3,7 @@ package eus.ehu.tss;
 import ij.IJ;
 import ij.ImagePlus;
 import weka.classifiers.lazy.IBk;
+import weka.classifiers.trees.RandomForest;
 
 import java.util.ArrayList;
 
@@ -11,7 +12,7 @@ public class TestColorSuperpixelSegmentation {
     public static void main(final String[] args){
         ImagePlus inputImage = IJ.openImage( TestSuperpixelSegmentation.class.getResource( "/TMA.png" ).getFile() );
         inputImage.show();
-        ImagePlus labelImage = IJ.openImage( TestSuperpixelSegmentation.class.getResource( "/TMA-Segmentation.png").getFile() );
+        ImagePlus labelImage = IJ.openImage( TestSuperpixelSegmentation.class.getResource( "/TMA-Segmentation-16-bit.zip").getFile() );
         labelImage.show();
 
         // Use all features
@@ -32,19 +33,17 @@ public class TestColorSuperpixelSegmentation {
         classes.add( "Stained-Tumor" );
 
         // Define classifier
-        IBk exampleClassifier = new IBk();
+        RandomForest exampleClassifier = new RandomForest();
         TrainableSuperpixelSegmentation test =
                 new TrainableSuperpixelSegmentation( inputImage, labelImage,
                         selectedFeatures, exampleClassifier, classes );
-        System.out.println(test.getFeaturesByRegion());
+        //System.out.println(test.getFeaturesByRegion());
 
         // Define training regions
-        int[] noStained = new int[4];
-        noStained[0] = 38; noStained[1]=108; noStained[2]=163;noStained[3]=223;
-        int[] stainedTum = new int[4];
-        stainedTum[0] = 34; stainedTum[1]=60;stainedTum[2]=181;stainedTum[3]=173;
-        int[] background = new int[4];
-        background[0]=1;background[1]=211;background[2]=35;background[3]=226;
+        int[] noStained = new int[]{ 176, 2111, 1322, 2298 };
+        int[] stainedTum = new int[]{ 416, 591, 2013, 2024 };
+        int[] background = new int[]{ 1, 360, 2742, 2795 };
+
         ArrayList<int[]> tags = new ArrayList<>();
         tags.add(background);
         tags.add(noStained);
