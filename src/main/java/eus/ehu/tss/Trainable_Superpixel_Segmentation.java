c5d3d7a471fd0f17b7403f10455d4c93da225d20
Josu <96jsalinas@gmail.com>
2018-04-09 14:04:03 +0300

    Changed plugin to add image panel and button without functionality

--- src/main/java/eus/ehu/tss/Trainable_Superpixel_Segmentation.java
+++ src/main/java/eus/ehu/tss/Trainable_Superpixel_Segmentation.java
@@ -11,180 +11,83 @@
 package eus.ehu.tss;
 
 import ij.IJ;
-import ij.ImageJ;
 import ij.ImagePlus;
-import ij.gui.GenericDialog;
-import ij.plugin.filter.PlugInFilter;
-import ij.process.ImageProcessor;
+import ij.WindowManager;
+import ij.gui.ImageCanvas;
+import ij.gui.StackWindow;
+import ij.plugin.PlugIn;
 
-/**
- * A template for processing each pixel of either
- * GRAY8, GRAY16, GRAY32 or COLOR_RGB images.
- *
- * @author Johannes Schindelin
- */
-public class Trainable_Superpixel_Segmentation implements PlugInFilter {
-	protected ImagePlus image;
+import javax.swing.*;
+import java.awt.*;
 
-	// image property members
-	private int width;
-	private int height;
 
-	// plugin parameters
-	public double value;
-	public String name;
+public class Trainable_Superpixel_Segmentation implements PlugIn {
 
-	@Override
-	public int setup(String arg, ImagePlus imp) {
-		if (arg.equals("about")) {
-			showAbout();
-			return DONE;
-		}
+    private CustomWindow win;
+    private ImagePlus inputImage;
+    private ImagePlus supImage;
 
-		image = imp;
-		return DOES_8G | DOES_16 | DOES_32 | DOES_RGB;
-	}
 
-	@Override
-	public void run(ImageProcessor ip) {
-		// get width and height
-		width = ip.getWidth();
-		height = ip.getHeight();
+    private class CustomWindow extends StackWindow
+    {
+        private Panel all = new Panel();
+        private JPanel buttonsPanel = new JPanel();
+        private JButton clusterizeButton = null;
+        CustomWindow(ImagePlus imp)
+        {
+            super(imp, new ImageCanvas(imp));
+            final ImageCanvas canvas = (ImageCanvas) getCanvas();
+            GridBagLayout layout = new GridBagLayout();
+            GridBagConstraints allConstraints = new GridBagConstraints();
+            all.setLayout(layout);
+            allConstraints.anchor = GridBagConstraints.CENTER;
+            allConstraints.fill = GridBagConstraints.BOTH;
+            allConstraints.gridwidth = 2;
+            allConstraints.gridheight = 1;
+            allConstraints.gridx = 0;
+            allConstraints.gridy = 0;
+            allConstraints.weightx = 0;
+            allConstraints.weighty = 0;
+            all.add(canvas,allConstraints);
+            allConstraints.gridy++;
 
-		if (showDialog()) {
-			process(ip);
-			image.updateAndDraw();
-		}
-	}
+            clusterizeButton = new JButton("Clusterize");
+            buttonsPanel.add(clusterizeButton);
+            all.add(buttonsPanel,allConstraints);
 
-	private boolean showDialog() {
-		GenericDialog gd = new GenericDialog("Process pixels");
 
-		// default value is 0.00, 2 digits right of the decimal point
-		gd.addNumericField("value", 0.00, 2);
-		gd.addStringField("name", "John");
+            GridBagLayout wingb = new GridBagLayout();
+            GridBagConstraints winc = new GridBagConstraints();
+            winc.anchor = GridBagConstraints.CENTER;
+            winc.fill = GridBagConstraints.BOTH;
+            winc.weightx = 0;
+            winc.weighty = 0;
+            setLayout( wingb );
+            add( all, winc );
+            pack();
+        }
+    }
 
-		gd.showDialog();
-		if (gd.wasCanceled())
-			return false;
+    @Override
+    public void run(String s) {
 
-		// get entered values
-		value = gd.getNextNumber();
-		name = gd.getNextString();
+        inputImage =IJ.openImage();
+        supImage = IJ.openImage();
+        if(inputImage == null ||supImage == null){
+            IJ.error("Error when opening image");
+        }else {
+            win = new CustomWindow(inputImage);
+        }
 
-		return true;
-	}
+    }
+    public static void main(String[] args){
+        Class<?> clazz = Trainable_Superpixel_Segmentation.class;
+        String url = clazz.getResource("/" + clazz.getName().replace('.', '/') + ".class").toString();
+        String pluginsDir = url.substring("file:".length(), url.length() - clazz.getName().length() - ".class".length());
+        System.setProperty("plugins.dir", pluginsDir);
+        IJ.runPlugIn(clazz.getName(),"");
 
-	/**
-	 * Process an image.
-	 * <p>
-	 * Please provide this method even if {@link ij.plugin.filter.PlugInFilter} does require it;
-	 * the method {@link ij.plugin.filter.PlugInFilter#run(ij.process.ImageProcessor)} can only
-	 * handle 2-dimensional data.
-	 * </p>
-	 * <p>
-	 * If your plugin does not change the pixels in-place, make this method return the results and
-	 * change the {@link #setup(java.lang.String, ij.ImagePlus)} method to return also the
-	 * <i>DOES_NOTHING</i> flag.
-	 * </p>
-	 *
-	 * @param image the image (possible multi-dimensional)
-	 */
-	public void process(ImagePlus image) {
-		// slice numbers start with 1 for historical reasons
-		for (int i = 1; i <= image.getStackSize(); i++)
-			process(image.getStack().getProcessor(i));
-	}
+    }
 
-	// Select processing method depending on image type
-	public void process(ImageProcessor ip) {
-		int type = image.getType();
-		if (type == ImagePlus.GRAY8)
-			process( (byte[]) ip.getPixels() );
-		else if (type == ImagePlus.GRAY16)
-			process( (short[]) ip.getPixels() );
-		else if (type == ImagePlus.GRAY32)
-			process( (float[]) ip.getPixels() );
-		else if (type == ImagePlus.COLOR_RGB)
-			process( (int[]) ip.getPixels() );
-		else {
-			throw new RuntimeException("not supported");
-		}
-	}
 
-	// processing of GRAY8 images
-	public void process(byte[] pixels) {
-		for (int y=0; y < height; y++) {
-			for (int x=0; x < width; x++) {
-				// process each pixel of the line
-				// example: add 'number' to each pixel
-				pixels[x + y * width] += (byte)value;
-			}
-		}
-	}
-
-	// processing of GRAY16 images
-	public void process(short[] pixels) {
-		for (int y=0; y < height; y++) {
-			for (int x=0; x < width; x++) {
-				// process each pixel of the line
-				// example: add 'number' to each pixel
-				pixels[x + y * width] += (short)value;
-			}
-		}
-	}
-
-	// processing of GRAY32 images
-	public void process(float[] pixels) {
-		for (int y=0; y < height; y++) {
-			for (int x=0; x < width; x++) {
-				// process each pixel of the line
-				// example: add 'number' to each pixel
-				pixels[x + y * width] += (float)value;
-			}
-		}
-	}
-
-	// processing of COLOR_RGB images
-	public void process(int[] pixels) {
-		for (int y=0; y < height; y++) {
-			for (int x=0; x < width; x++) {
-				// process each pixel of the line
-				// example: add 'number' to each pixel
-				pixels[x + y * width] += (int)value;
-			}
-		}
-	}
-
-	public void showAbout() {
-		IJ.showMessage("ProcessPixels",
-			"a template for processing each pixel of an image"
-		);
-	}
-
-	/**
-	 * Main method for debugging.
-	 *
-	 * For debugging, it is convenient to have a method that starts ImageJ, loads
-	 * an image and calls the plugin, e.g. after setting breakpoints.
-	 *
-	 * @param args unused
-	 */
-	public static void main(String[] args) {
-		// set the plugins.dir property to make the plugin appear in the Plugins menu
-		Class<?> clazz = Trainable_Superpixel_Segmentation.class;
-		String url = clazz.getResource("/" + clazz.getName().replace('.', '/') + ".class").toString();
-		String pluginsDir = url.substring("file:".length(), url.length() - clazz.getName().length() - ".class".length());
-		System.setProperty("plugins.dir", pluginsDir);
-
-		// start ImageJ
-		new ImageJ();
-
-		// open the Clown sample
-		ImagePlus image = IJ.openImage("http://imagej.net/images/clown.jpg");
-		image.show();
-
-		// run the plugin
-		IJ.runPlugIn(clazz.getName(), "");
-	}
 }
