811b42bb42f1d617c0648997fa8c09f4dbe81f89
Josu <96jsalinas@gmail.com>
2018-04-13 11:59:55 +0300

    Added annotation panel and action listeners

--- src/main/java/eus/ehu/tss/Trainable_Superpixel_Segmentation.java
+++ src/main/java/eus/ehu/tss/Trainable_Superpixel_Segmentation.java
@@ -22,6 +22,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -32,6 +34,8 @@ public class Trainable_Superpixel_Segmentation implements PlugIn {
     private ImagePlus inputImage;
     private ImagePlus supImage;
     private final ExecutorService exec = Executors.newFixedThreadPool(1);
+    private int numClasses = 2;
+    private  java.awt.List[] exampleList = new java.awt.List[numClasses];
 
     private class CustomWindow extends StackWindow
     {
@@ -39,7 +43,7 @@ public class Trainable_Superpixel_Segmentation implements PlugIn {
         private JPanel controlsPanel = new JPanel();
         private JPanel classifierPanel = new JPanel();
         private JPanel resultPanel = new JPanel();
-        private JPanel classPanel = new JPanel();
+        private JPanel annotationsPanel = new JPanel();
         private JButton trainClassButton = null;
         private JButton loadClassButton = null;
         private JButton applyClassButton = null;
@@ -48,8 +52,77 @@ public class Trainable_Superpixel_Segmentation implements PlugIn {
         private JButton probButton = null;
         private JButton resButton = null;
         private JButton overlayButton = null;
+        private JButton [] addExampleButton = new JButton[numClasses];
 
 
+        private ActionListener listener = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                final String command = e.getActionCommand();
+
+                exec.submit(new Runnable() {
+                    @Override
+                    public void run() {
+                        if(e.getSource() == trainClassButton)
+                        {
+                            runStopTraining(command);
+                        }
+                        else if(e.getSource() == overlayButton){
+                            toggleOverlay();
+                        }
+                        else if(e.getSource() == resButton){
+                           createResult();
+                        }
+                        else if(e.getSource() == probButton){
+                            showProbability();
+                        }
+                        else if(e.getSource() == plotButton){
+                            showPlot();
+                        }
+                        else if(e.getSource() == applyClassButton){
+                            applyClassifier();
+                        }
+                        else if(e.getSource() == loadClassButton){
+                            loadClassifier();
+                        }
+                        else if(e.getSource() == settButton){
+                            showSettingsDialog();
+                        }
+                        else{
+                            for(int i = 0; i < numClasses; i++)
+                            {
+                                if(e.getSource() == exampleList[i])
+                                {
+                                    deleteSelected(e);
+                                    break;
+                                }
+                                if(e.getSource() == addExampleButton[i])
+                                {
+                                    addExamples(i);
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                });
+            }
+        };
+
+        private ItemListener itemListener = new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                exec.submit(new Runnable() {
+                    @Override
+                    public void run() {
+                        for(int i = 0; i < numClasses; i++)
+                        {
+                            if(e.getSource() == exampleList[i])
+                                listSelected(e, i);
+                        }
+                    }
+                });
+            }
+        };
 
         CustomWindow(ImagePlus imp)
         {
@@ -108,6 +181,19 @@ public class Trainable_Superpixel_Segmentation implements PlugIn {
             resultConstraints.weightx = 0;
             resultConstraints.weighty = 0;
 
+
+            // Annotations panel
+            GridBagLayout boxAnnotation = new GridBagLayout();
+            annotationsPanel.setBorder(BorderFactory.createTitledBorder("Labels"));
+            annotationsPanel.setLayout(boxAnnotation);
+            GridBagConstraints annotationsConstraints = new GridBagConstraints();
+            annotationsConstraints.anchor = GridBagConstraints.NORTHWEST;
+            annotationsConstraints.fill = GridBagConstraints.HORIZONTAL;
+            annotationsConstraints.gridwidth = 1;
+            annotationsConstraints.gridheight = 1;
+            annotationsConstraints.gridx = 0;
+            annotationsConstraints.gridy = 0;
+
             //Classifier panel buttons
             trainClassButton = new JButton("Train classifier");
             classifierPanel.add(trainClassButton,classifierConstraints);
@@ -140,7 +226,41 @@ public class Trainable_Superpixel_Segmentation implements PlugIn {
             controlsPanel.add(resultPanel,controlConstraints);
             controlConstraints.gridx++;
 
+            //Annotations panel
+            for(int i=0; i<numClasses;++i){
+                exampleList[i].addActionListener(listener);
+                exampleList[i].addItemListener(itemListener);
+                addExampleButton[i] = new JButton("Add to class "+i);
+                addExampleButton[i].setToolTipText("Add markings of label 'class "+i+"'");
+
+                annotationsConstraints.insets = new Insets(5,5,6,6);
+
+                annotationsPanel.add(addExampleButton[i],annotationsConstraints);
+                annotationsConstraints.gridy++;
+
+                annotationsConstraints.insets = new Insets(0,0,0,0);
+
+                annotationsPanel.add(exampleList[i], annotationsConstraints);
+                annotationsConstraints.gridy++;
+            }
+            addExampleButton[0].setSelected(true);
+
+            //Add listeners
+            for(int i = 0; i< numClasses; ++i){
+                addExampleButton[i].addActionListener(listener);
+            }
+            trainClassButton.addActionListener(listener);
+            loadClassButton.addActionListener(listener);
+            applyClassButton.addActionListener(listener);
+            settButton.addActionListener(listener);
+            plotButton.addActionListener(listener);
+            probButton.addActionListener(listener);
+            resButton.addActionListener(listener);
+            overlayButton.addActionListener(listener);
+
             all.add(controlsPanel,allConstraints);
+            allConstraints.gridx++;
+            all.add(annotationsPanel,allConstraints);
 
             GridBagLayout wingb = new GridBagLayout();
             GridBagConstraints winc = new GridBagConstraints();
@@ -154,17 +274,70 @@ public class Trainable_Superpixel_Segmentation implements PlugIn {
         }
     }
 
+
+    void runStopTraining(final String command){
+        System.out.println("To be implemented: Train classifier");
+    }
+
+    void applyClassifier(){
+        System.out.println("To be implemented: Apply classifier");
+    }
+
+    void loadClassifier(){
+        System.out.println("To be implemented: Load classifier");
+    }
+
+    void showSettingsDialog(){
+        System.out.println("To be implemented: Show settings dialog");
+    }
+
+    void showPlot(){
+        System.out.println("To be implemented: Show Plot");
+    }
+
+    void showProbability(){
+        System.out.println("To be implemented: Show probability");
+    }
+
+    void createResult(){
+        System.out.println("To be implemented: Create result");
+    }
+
+    void toggleOverlay(){
+        System.out.println("To be implemented: Toggle overlay");
+    }
+
+    void deleteSelected(final ActionEvent e){
+        System.out.println("To be implemented: Delete selected "+e.toString());
+    }
+
+    void addExamples(int i){
+        System.out.println("To be implemented: Add examples "+i);
+    }
+
+    void listSelected(final ItemEvent e, final int i)
+    {
+       System.out.println("To be implemented: List selected: e: "+e.toString()+" i: "+i);
+    }
+
+
+
+
+
     @Override
     public void run(String s) {
 
         inputImage =IJ.openImage();
         supImage = IJ.openImage();
+        for(int i=0; i<numClasses; ++i){
+            exampleList[i] = new java.awt.List(5);
+            exampleList[i].setForeground(Color.blue);
+        }
         if(inputImage == null ||supImage == null){
             IJ.error("Error when opening image");
         }else {
             win = new CustomWindow(inputImage);
             Toolbar.getInstance().setTool( Toolbar.POINT );
-
         }
 
     }
