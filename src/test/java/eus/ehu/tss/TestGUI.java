19a8e72bce6f5274050a73cb0d80b2cf37c4025b
Ignacio Arganda-Carreras <ignacio.arganda@gmail.com>
2019-03-04 12:43:56 +0100

    Add missing javadoc comments.

--- src/test/java/eus/ehu/tss/TestGUI.java
+++ src/test/java/eus/ehu/tss/TestGUI.java
@@ -3,7 +3,15 @@ package eus.ehu.tss;
 
 import ij.ImageJ;
 
+/**
+ * This class allows testing the plugin GUI.
+ *
+ */
 public class TestGUI {
+    /**
+     * Main class to test GUI.
+     * @param args ImageJ arguments
+     */
     public static void main( final String[] args )
     {
         ImageJ.main( args );
